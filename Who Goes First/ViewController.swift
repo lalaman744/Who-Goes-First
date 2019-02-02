dc0d2f0f54e21194242786b43628112b4fb34b28
Josh Kleinschmidt <lalaman744@hotmail.com>
2019-02-02 11:37:40 -0600

    Initial Commit

---
+++ Who Goes First/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Who Goes First
+//
+//  Created by Josh Kleinschmidt on 2/2/19.
+//  Copyright © 2019 Josh Kleinschmidt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
